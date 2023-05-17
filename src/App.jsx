a84126ff7f46c9a747e0ca19f1382eccd4f77e7e
jelimo-charity <charityjelimo893@gmail.com>
2023-05-17 08:56:09 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Main from '../components/main'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Main from './components/main'
 
 
 function App() {
